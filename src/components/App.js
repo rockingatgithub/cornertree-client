ab68863454406c12c9d9052b6587f0cc4c0d2df1
rockingatgithub <contactwithsid@gmail.com>
2020-10-17 01:00:29 +0530

    navbar added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,41 @@
 import React, { Component } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
 class App extends Component {
   render() {
-    return <div>This is the homepage.... On way to complete</div>;
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Navbar bg="light" expand="lg">
+              <Navbar.Brand href="#home">Naukri.com</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="#home">Jobs</Nav.Link>
+                  <Nav.Link href="#link">Recruiters</Nav.Link>
+                  <Nav.Link href="#home">Companies</Nav.Link>
+                  <Nav.Link href="#home">Tools</Nav.Link>
+                  <Nav.Link href="#home">Services</Nav.Link>
+                  <Nav.Link href="#home">More</Nav.Link>
+                  <Nav.Link href="#home">Login</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
 
