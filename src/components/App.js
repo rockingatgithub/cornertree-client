eda78017a1beb71fdba656b7060afb8ec8937932
rockingatgithub <contactwithsid@gmail.com>
2020-10-17 00:13:48 +0530

    first component

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div>This is the homepage.... On way to complete</div>;
+  }
+}
+
+export default App;
